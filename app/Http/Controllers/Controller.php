1acd19fcf2a6ba2a434fb7d31c114ab0b807c256
AlimulDevs <alimuldev@gmail.com>
2023-01-08 23:57:12 +0700

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class Controller extends BaseController
 {
@@ -21,6 +23,40 @@ class Controller extends BaseController
 
 
         return view("create");
+    }
+    public function create(Request $request)
+    {
+        $file = $request->file('foto_leader');
+        if ($file == null) {
+            return redirect('/createIndex')->with("failed", "failed Create Data");
+        }
+        $nameFile = time() . $file->getClientOriginalName();
+        if ($file->getClientMimeType() == 'application/pdf') {
+            return redirect('/createIndex')->with("failed", "File harus berupa png or jpg");
+        }
+        // isi dengan nama folder tempat kemana file diupload
+        $tujuan_upload = 'images';
+
+
+        // upload file
+        $file->move($tujuan_upload, $nameFile);
+        $nameFile = url('/images/' . $nameFile);
+
+
+
+
+        DB::table("mentorings")->insert([
+            "project_name" => $request->project_name,
+            "client" => $request->client,
+            "name_leader" => $request->name_leader,
+            "email_leader" => $request->email_leader,
+            "foto_leader" => $nameFile,
+            "start_date" => $request->start_date,
+            "end_date" => $request->end_date,
+            "progress" => $request->progress,
+        ]);
+
+        return redirect("/createIndex")->with("success", "Success Create Data");
     }
     public function delete($id)
     {
