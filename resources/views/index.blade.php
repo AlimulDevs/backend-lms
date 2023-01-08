8734d183dc82639aab149947ae2184514940c3d9
AlimulDevs <alimuldev@gmail.com>
2023-01-08 19:57:54 +0700

    add

---
+++ resources/views/index.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-body">
+                <table class="table">
+                    <thead class="text-center">
+                        <th>PROJECT NAME</th>
+                        <th>CLIENT</th>
+                        <th>PROJECT LEADER</th>
+                        <th>START DATE</th>
+                        <th>END DATE</th>
+                        <th>PROGRESS</th>
+                        <th>ACTION</th>
+
+                    </thead>
+                    <tbody class="text-center">
+                        <tr>
+                            <td>tes</td>
+                            <td>tes</td>
+                            <td>tes</td>
+                            <td>tes</td>
+                            <td>tes</td>
+                            <td>tes</td>
+                            <td>tes</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row-1 text-end me-3">
+            <div class="col">
+                Created by :
+            </div>
+
+            <div class="col">
+                Nur Alimul Haq
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+
+
+
+
+</html>
